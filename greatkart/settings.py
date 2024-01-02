a20a141243448b16836657eaf854e1b8c97c0033
tettehnarh <leslienarh@outlook.com>
2024-01-02 22:40:06 +0000

    django auth

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -133,6 +133,20 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# SMTP configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'tettehlnarh@gmail.com'
+EMAIL_HOST_PASSWORD = 'qvrbizvkffzsnmik'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
