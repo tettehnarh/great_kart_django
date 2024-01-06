4569b210390ff074d40ec544df6f59f78fd90943
tettehnarh <leslienarh@outlook.com>
2024-01-06 23:50:01 +0000

    cart and checkout page for loggein users

--- accounts/views.py
+++ accounts/views.py
@@ -13,6 +13,9 @@ from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 from django.http import HttpResponse
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
+import requests
 
 
 
@@ -61,9 +64,54 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                print(is_cart_item_exists)
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    product_variation = [] # list for storing product variations
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation)) # append variation to the product_variation list
+
+                    # get the cart items from the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list= [] # ex_var_list = existing variation list
+                    id = [] # id = list for storing the id of the cart item
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    # check if the product variation is in the existing variation list
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(product__id=pr[0], id=item_id)
+                            item.quantity +=1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+            except:
+                pass
             auth.login(request, user)
             messages.success(request, 'You are now logged in')
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                # next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credentials')
             return redirect('login')      
--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from store.models import Product, Variation
+from django.contrib.auth.models import User
+from accounts.models import Account
 
 # Create your models here.
 class Cart(models.Model):
@@ -14,9 +16,10 @@ class Cart(models.Model):
         return self.cart_id
     
 class CartItem(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True) # on_delete=models.CASCADE means that if the user is deleted, the cart item will also be deleted. It is a good practice to delete the cart item when the user is deleted. It is a good practice
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     variations = models.ManyToManyField(Variation, blank=True) # blank=True means that the field is optional. It is not required to add variations to the cart item. It is optional because it is not required to add variations to the cart item. It is optional because it is not required to add
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
